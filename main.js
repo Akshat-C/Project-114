c973e8021a8a3a1e0f0d6f50183fecc46f42bea0
Akshat-C <85225437+Akshat-C@users.noreply.github.com>
2022-02-15 20:40:27 +1100

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
+moustache = 0;
+lipstick = 0;
+noseX = "";
+noseY = "";
+tint_value = "";
 function preload()
-{}
+{
+    i1 = loadImage('https://i.postimg.cc/V6sTdsBR/Moustache.png');
+    i2 = loadImage('https://i.postimg.cc/6p3FWpcf/imageedit-1-5826119281.png')
+}
 
 function setup(){
     canvas = createCanvas(400, 350);
@@ -16,6 +24,19 @@ function setup(){
 function draw()
 {
     image(video, 0, 0, 400, 350);
+   
+    if (moustache == 1)
+    {
+        image(i1, noseX, noseY, 70, 60);
+        tint(tint_value);
+    } 
+    
+    if (lipstick == 2)
+    {
+        image(i2, noseX+10, noseY+30, 50, 40);
+        tint(tint_value);
+    }
+    
 }
 
 function save_img()
@@ -34,8 +55,23 @@ function gotPoses(results)
     {
         console.log(results);
 
-        noseX = results[0].pose.nose.x;
-        noseY = results[0].pose.nose.y;
+        noseX = results[0].pose.nose.x-35;
+        noseY = results[0].pose.nose.y-10;
         console.log("Nose X is: " + noseX + "Nose Y is: " + noseY);
     }
+}
+
+function change_tint()
+{
+    tint_value = document.getElementById("tint_color").value;
+}
+
+function add_moustache()
+{
+    moustache = 1;
+}
+
+function add_lipstick()
+{
+    lipstick = 2; 
 }
